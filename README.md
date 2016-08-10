83331a4444e40e86e93d705cedb07a44f837bd08
Caleb Paul and Stephen Burden <caleb@>
2016-08-10 14:02:01 -0700

    update README and used branching to return results to user

---
+++ README.md
@@ -0,0 +1,33 @@
+Epicodus README Template
+
+# _Basic Calculator_
+
+#### _A Calculator that will add, subtract, multiply and divide, 12/10/2016_
+
+#### By _**Stephen Burden and Caleb Paul**_
+
+## Description
+
+_A web page that has a calculator that will add, subtract, multiply and divide from two numbers a user will submit._
+
+## Setup/Installation Requirements
+
+* _Clone from github and open index.html in a web browser_
+
+## Known Bugs
+
+_N/A_
+
+## Support and contact details
+
+_Tweet @calebpaulmusic_
+
+## Technologies Used
+
+_Javascript, JQueries, Bootstrap_
+
+### License
+
+*The MIT License*
+
+Copyright (c) 2016 **_Stephen Burden and Caleb Paul_**
--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,32 @@ multiply = function(number1, number2) {
 },
 divide = function(number1, number2) {
   return number1 / number2;
-};
+},
+buttonName = "";
 
 //UI/front-end logic:
-$(document).ready(function() {
+$( document ).ready(function() {
 
-  $("form#add").submit(function(event) {
-  event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
+  $("button").click(function() {
+    buttonName=this.id;
+  });
+
+  $("#calc_form").submit(function(event) {
+    event.preventDefault();
+      var number1 = parseInt($("#input1").val());
+      var number2 = parseInt($("#input2").val());
+      var result = "";
+
+      if (buttonName==="add") {
+        result = add(number1, number2)
+      } else if (buttonName==="subtract") {
+        result = subtract(number1, number2)
+      } else if (buttonName==="multiply") {
+        result = multiply(number1, number2)
+      } else {
+        result = divide(number1, number2)
+      }
+
+      $(".output").text(result);
   });
 });
